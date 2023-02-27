0752ed25d295463c6ae10137a70b3deb46ec5e43
ELDER ALBERTO ZAPATA BOLIVAR <elderzapata265446@correo.itm.edu.co>
2023-02-26 19:01:05 -0500

    Creation new abstract class called Employee

---
+++ MyFirstOOPCode-Saturday/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstOOPCode_Saturday
+{
+    #region Propierties
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDate { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+    #endregion
+
+    #region Methods
+    public abstract int GetValueToPay();
+
+    public override string ToString() 
+    {
+        return $"----EMPLOYEE----\n\t " +
+            $"ID:           {Id} \n\t " +
+            $"First Name:   {FirstName} \n\t " +
+            $"LastName:     {LastName} \n\t " +
+            $"Birth Date:   {BirthDate} \n\t " +
+            $"Hiring Date:  {HiringDate} \n\t " +
+            $"Is Active:    {IsActive}";
+        
+    }
+
+    #endregion
+
+}
